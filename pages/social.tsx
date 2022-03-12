3e97a8cd1f585d700f7038a33defe3fe3d18dd19
Blizzcane <blizzcane@gmail.com>
2022-03-11 22:17:15 -0600

    learning tsx with nextjs

---
+++ pages/social.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export default function Social() {
+  const [subs, setSubs] = useState(6000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSubs((sub) => subs + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+    console.log(subs);
+  return <h1>Subs: {subs}</h1>;
+}
